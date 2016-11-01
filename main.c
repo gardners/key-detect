af42b41a1e52a06a6bd215e1d097afa2bff13048
gardners <paul@servalproject.org>
2016-11-01 11:53:15 +0100

    correct parameterisation of vector density

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ long long gettime_us()
 
 int main(int argc,char **argv)
 {
-  printf("Testing insertion speed of %d keys...\n",VECTOR_LENGTH>>4);
+  printf("Testing insertion speed of %d keys...\n",MAX_KEYS);
 
   FILE *f=fopen("/dev/random","r");
 
@@ -29,7 +29,7 @@ int main(int argc,char **argv)
   long long start=gettime_us();
   unsigned char key[64];
   time_t last_time=time(0);
-  for(int i=0;i<VECTOR_LENGTH>>4;i++) {
+  for(int i=0;i<MAX_KEYS;i++) {
     if (fread(key,64,1,f)!=1) {
       fprintf(stderr,"Failed to read 64 bytes from /dev/random.\n");
     }    
@@ -38,9 +38,9 @@ int main(int argc,char **argv)
   long long end=gettime_us();
   long long key_gen_time=end-start;
   printf("Key generation (without inserting) of %d keys required %lldusec.\n",
-	 VECTOR_LENGTH>>4,key_gen_time);
+	 MAX_KEYS,key_gen_time);
   start=gettime_us();
-  for(int i=0;i<VECTOR_LENGTH>>4;i++) {
+  for(int i=0;i<MAX_KEYS;i++) {
     if (fread(key,64,1,f)!=1) {
       fprintf(stderr,"Failed to read 64 bytes from /dev/random.\n");
     }
@@ -53,9 +53,9 @@ int main(int argc,char **argv)
   end=gettime_us();
   long long key_insert_time=end-start;
   printf("Key generation (with inserting) of %d keys required %lldusec.\n",
-	 VECTOR_LENGTH>>4,key_insert_time);
+	 MAX_KEYS,key_insert_time);
   printf("Subtracting key generation time, key insertion required %.1f usec/key.\n",
-	 (key_insert_time-key_gen_time)*1.0/(VECTOR_LENGTH>>4));
+	 (key_insert_time-key_gen_time)*1.0/MAX_KEYS);
 
   // Measure density in vectors
   for(int v=0;v<NUM_VECTORS;v++) {
@@ -63,7 +63,7 @@ int main(int argc,char **argv)
     for(int i=0;i<VECTOR_LENGTH;i++)
       if (get_vector_bit(v,i)) bitcount++;
     float density=bitcount*100.0/VECTOR_LENGTH;
-    if (density>=(100.0/16)) {
+    if (density>=(MAX_KEYS*100.0/VECTOR_LENGTH)) {
       printf("Vector #%d has erroneously high density of %.2f%%.\n",
 	     v,density);
     }
